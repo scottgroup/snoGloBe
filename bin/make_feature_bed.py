aef8284e3d622b1b0a622c806b37575b862e4c61
Gabrielle DF <gabrielle.deschamps-francoeur@usherbrooke.ca>
2020-12-10 16:30:20 -0500

    sort gtf

--- bin/make_feature_bed.py
+++ bin/make_feature_bed.py
@@ -5,17 +5,13 @@ import pandas as pd
 
 def intron_bed(df_gtf):
     df_gtf = df_gtf[df_gtf.feature == 'exon']
+    df_gtf = df_gtf.sort_values(['seqname', 'gene_id', 'transcript_id', 'start', 'end'])
     df_gtf = df_gtf[['seqname', 'start', 'end', 'strand', 'transcript_id', 'score']]
     df_gtf['next_start'] = df_gtf.start.shift(-1)
     df_gtf['next_trx'] = df_gtf.transcript_id.shift(-1)
     df_introns = df_gtf[df_gtf.transcript_id == df_gtf.next_trx].copy(deep=True)
-    df_introns.loc[df_introns.strand == '+', 'intron_start'] = df_introns.end
-    df_introns.loc[df_introns.strand == '+', 'intron_end'] = df_introns.next_start
-    df_introns.loc[df_introns.strand == '-', 'intron_end'] = df_introns.end
-    df_introns.loc[df_introns.strand == '-', 'intron_start'] = df_introns.next_start
-    df_introns[['intron_end']] = df_introns[['intron_end']].astype(int)
-    df_introns['intron_end'] -= 1
-    df_introns['intron_start'] += 1
+    df_introns['intron_start'] = df_introns.end.astype(int) + 1
+    df_introns['intron_end'] = df_introns.next_start.astype(int) - 1
     df_introns['feature'] = 'intron'
     return df_introns[['seqname', 'intron_start', 'intron_end', 'feature', 'score', 'strand']]
 
@@ -31,4 +27,5 @@ def make_bed(df_gtf, bedfile):
     df_bed['end'] += 1  # to bypass the fact that if only the last nucleotide overlaps, the overlap is not reported
     df_bed = df_bed.sort_values(['seqname', 'start', 'end'])
     df_bed[['start', 'end']] = df_bed[['start', 'end']].astype(int)
+    df_bed = df_bed.drop_duplicates()
     df_bed.to_csv(bedfile, sep='\t', header=False, index=False)
